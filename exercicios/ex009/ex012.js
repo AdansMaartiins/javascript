74c6b2fe8aa9eda9c23e7bf8f3eecc919d2a495b
AdansMaartiins <97614889+AdansMaartiins@users.noreply.github.com>
2022-05-20 16:32:17 -0300

    atualizei os exercícios

---
+++ exercicios/ex009/ex012.js
@@ -0,0 +1,10 @@
+var hora = 8
+console.log('Agora são exatamente ' + 8 + ' horas.')
+if (hora < 12) {
+    console.log('Bom Dia!')
+} else if (hora <= 18) {
+    console.log('Boa Tarde!')
+} else{
+    console.log('Boa noite!')
+} 
+   
